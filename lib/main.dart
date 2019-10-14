9bcce4d2cfab7865986f7b5f53387ba65e9aa0ac
renaldi <renalazyy@gmail.com>
2019-10-14 14:07:12 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter TabBar',
+      home: new Home(),
+      theme: new ThemeData(primaryColor: Colors.black),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    tabController = new TabController(length: 2, vsync: this);
+
+    var tabBarItem = new TabBar(
+      tabs: [
+        new Tab(
+          icon: new Icon(Icons.list),
+        ),
+        new Tab(
+          icon: new Icon(Icons.grid_on),
+        ),
+      ],
+      controller: tabController,
+      indicatorColor: Colors.white,
+    );
+
+    var listItem = new ListView.builder(
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: new Card(
+            elevation: 5.0,
+            child: new Container(
+              alignment: Alignment.center,
+              margin: new EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: new Text("ListItem $index"),
+            ),
+          ),
+          onTap: () {
+            showDialog(
+                barrierDismissible: false,
+                context: context,
+                child: new CupertinoAlertDialog(
+                  title: new Column(
+                    children: <Widget>[
+                      new Text("ListView"),
+                      new Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                  content: new Text("Selected Item $index"),
+                  actions: <Widget>[
+                    new FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: new Text("OK"))
+                  ],
+                ));
+          },
+        );
+      },
+    );
