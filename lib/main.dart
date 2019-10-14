bf87bb4cd04df1cbf6981a27bbc885877b17dbdc
renaldi <renalazyy@gmail.com>
2019-10-14 14:09:29 +0700

    gridview added

--- lib/main.dart
+++ lib/main.dart
@@ -79,3 +79,63 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         );
       },
     );
+
+    var gridView = new GridView.builder(
+        itemCount: 20,
+        gridDelegate:
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return new GestureDetector(
+            child: new Card(
+              elevation: 5.0,
+              child: new Container(
+                alignment: Alignment.center,
+                child: new Text('Item $index'),
+              ),
+            ),
+            onTap: () {
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                child: new CupertinoAlertDialog(
+                  title: new Column(
+                    children: <Widget>[
+                      new Text("GridView"),
+                      new Icon(
+                        Icons.favorite,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                  content: new Text("Selected Item $index"),
+                  actions: <Widget>[
+                    new FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: new Text("OK"))
+                  ],
+                ),
+              );
+            },
+          );
+        });
+
+    return new DefaultTabController(
+      length: 2,
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Flutter TabBar"),
+          bottom: tabBarItem,
+        ),
+        body: new TabBarView(
+          controller: tabController,
+          children: [
+            listItem,
+            gridView,
+          ],
+        ),
+      ),
+    );
+  }
+}
